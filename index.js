ce1cf293bfbf84557b3eabed99d437249578fbc0
Mellow <95871222+Mell-o@users.noreply.github.com>
2026-05-21 17:43:08 -0400

    built food menu

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ import { menuArray } from "./data.js";
 const foodMenuList = document.querySelector(".food-menu ul")
 
 function buildMenuItemsHtml() {
-    const menuItemsHtml = menuArray.map(({name, ingredients, price, emoji}) => {
+    foodMenuList.innerHTML = menuArray.map(({name, ingredients, price, emoji}) => {
         return `
-            <li class="menu-item">
-                <span class="food-emoji" role="img" aria-label="${name}">${emoji}</span>
-                <div class="menu-item-content">
-                    <h2>${name}</h2>
-                    <p class="food-ingredients">${ingredients}</p>
-                    <p class="food-price">${price}</p>
-                </div>
-                <button type="button" onclick="">+</button>
-            </li>
+                <li class="menu-item">
+                    <span class="food-emoji" role="img" aria-label="${name}">${emoji}</span>
+                    <div class="menu-item-content">
+                        <h2>${name}</h2>
+                        <p class="food-ingredients">${ingredients.join(", ")}</p>
+                        <p class="food-price">$${price}</p>
+                    </div>
+                    <button class="add-menu-item-btn" type="button" onclick="">+</button>
+                </li>
         `
-    })
+    }).join("")
 }
+
+buildMenuItemsHtml()
