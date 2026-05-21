8707e58d7d35fd3f191d25f42967c826c95707d0
Mellow <95871222+Mell-o@users.noreply.github.com>
2026-05-21 11:00:48 -0400

    Update index.js
    Create menu item template.

--- index.js
+++ index.js
@@ -2,4 +2,22 @@ import { menuArray } from "./data.js";
 
 const foodMenuList = document.querySelector(".food-menu ul")
 
-console.log(foodMenuList)
+function buildMenuItems() {
+    const menuItemsHtml = []
+    let menuItemHtml = ""
+
+    menuArray.forEach(({name, ingredients, price, emoji}) => {
+        menuItemHtml = `
+        <li class="menu-item">
+            <span class="food-emoji" role="img" aria-label="${name}">${emoji}</span>
+            <div class="menu-item-content">
+                <h2>${name}</h2>
+                <p class="food-ingredients">${ingredients}</p>
+                <p class="food-price">${price}</p>
+            </div>
+            <button type="button" onclick="">+</button>
+        </li>
+        `
+        menuItemsHtml.push(menuItemHtml)
+    })
+}
