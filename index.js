8a75fd80f9ec2f906e221689bed34a77dd23e8ef
Mellow <95871222+Mell-o@users.noreply.github.com>
2026-05-21 11:03:07 -0400

    Update index.js
    Switch menu item builder from a forEach to a map.

--- index.js
+++ index.js
@@ -2,22 +2,28 @@ import { menuArray } from "./data.js";
 
 const foodMenuList = document.querySelector(".food-menu ul")
 
-function buildMenuItems() {
-    const menuItemsHtml = []
-    let menuItemHtml = ""
-
-    menuArray.forEach(({name, ingredients, price, emoji}) => {
-        menuItemHtml = `
-        <li class="menu-item">
-            <span class="food-emoji" role="img" aria-label="${name}">${emoji}</span>
-            <div class="menu-item-content">
-                <h2>${name}</h2>
-                <p class="food-ingredients">${ingredients}</p>
-                <p class="food-price">${price}</p>
-            </div>
-            <button type="button" onclick="">+</button>
-        </li>
+function buildMenuItemsHtml() {
+    const menuItemsHtml = menuArray.map(({name, ingredients, price, emoji}) => {
+        return `
+            <li class="menu-item">
+                <span class="food-emoji" role="img" aria-label="${name}">${emoji}</span>
+                <div class="menu-item-content">
+                    <h2>${name}</h2>
+                    <p class="food-ingredients">${ingredients}</p>
+                    <p class="food-price">${price}</p>
+                </div>
+                <button type="button" onclick="">+</button>
+            </li>
         `
-        menuItemsHtml.push(menuItemHtml)
     })
 }
+
+                <li class="menu-item">
+                    <span class="food-emoji" role="img" aria-label="${name}"></span>
+                    <div class="menu-item-content">
+                        <h2>Pizza</h2>
+                        <p class="food-ingredients">pepperoni, mushroom, mozarella</p>
+                        <p class="food-price">$14</p>
+                    </div>
+                    <button type="button" onclick="">+</button>
+                </li>
